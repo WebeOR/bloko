f31e390ed48202378c103a6b7620e2b3a827fb08
AlexQuidditch <alexquidditch@gmail.com>
2019-08-08 02:30:06 +0300

    tryings to save scene elements

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,13 +12,16 @@
       max="10"
     />
 
+    <button @click="addCube">addCube</button>
+    <button @click="initScene">initScene</button>
+
     <div ref="scene" id="scene"></div>
   </div>
 </template>
 
 <script lang="ts">
 
-import { Component, Vue, Watch } from 'vue-property-decorator';
+import { Vue, Component, Watch } from 'vue-property-decorator';
 
 import {
   Scene,
@@ -34,6 +37,8 @@ import {
   LightShadow,
   PlaneBufferGeometry,
   ShadowMaterial,
+  MeshBasicMaterial,
+  Color,
 } from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
@@ -45,10 +50,12 @@ export default class Home extends Vue {
   SCENE: Scene = {};
   CAMERA: PerspectiveCamera = {};
   RENDERER: WebGLRenderer = {};
-  LIGHT: DirectionalLight = {};
+  LIGHT: SpotLight = {};
   CONTROLS: OrbitControls = {};
   GRID: GridHelper = {};
   AMBIENT_LIGHT: AmbientLight = {};
+
+  objects: Array<any> = [];
 
   @Watch('positionZ')
   onPositionZChange (val: number) {
@@ -63,6 +70,25 @@ export default class Home extends Vue {
     this.CONTROLS.update();
   }
 
+  private addCube () {
+    const geometry = new BoxGeometry( 2, 2, 2 );
+
+    const material = new MeshBasicMaterial({ color: 'red' });
+
+    const mesh = new Mesh( geometry, material );
+
+    mesh.position.x = 10;
+
+    //scene is global
+    this.SCENE.add(mesh);
+
+    console.log(this.SCENE.children);
+  }
+
+  private initScene () {
+    this.objects.forEach( obj => this.SCENE.add(obj) );
+  }
+
   async mounted () {
     await this.$nextTick();
 
@@ -71,26 +97,36 @@ export default class Home extends Vue {
     const geometry = new BoxGeometry(1, 2, 1);
     const material = new MeshPhongMaterial({ color: 'gray' });
     const cube = new Mesh( geometry, material );
+    const planeGeometry = new PlaneBufferGeometry(2000, 2000).rotateX(-Math.PI / 2);
+    const planeMaterial = new ShadowMaterial({ opacity: 0.2 });
 
     if (scene) {
       this.SCENE = new Scene();
+      this.SCENE.background = new Color(0xf0f0f0);
       
-      this.CAMERA = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+      this.CAMERA = new PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        10000
+      );
       this.CAMERA.position.z = 5;
 
-      this.LIGHT = new DirectionalLight('#fff', 1);
+      this.SCENE.add(new AmbientLight(0xf0f0f0));
+
+      this.LIGHT = new SpotLight('#fff', 1.5);
       this.LIGHT.position.set( 0, 1500, 200 );
       this.LIGHT.castShadow = true;
       this.LIGHT.shadow = new LightShadow( new PerspectiveCamera( 70, 1, 200, 2000 ) );
       this.LIGHT.shadow.bias = - 0.000222;
       this.LIGHT.shadow.mapSize.width = 1024;
       this.LIGHT.shadow.mapSize.height = 1024;
-      this.SCENE.add(this.LIGHT);
+      this.objects.push(this.LIGHT);
       
       this.GRID = new GridHelper( 100, 100, 'red', 'green' );
-      this.GRID.position.y = -1;
+      this.GRID.position.y = 0;
       this.GRID.material.opacity = 0.25;
-      this.GRID.material.transparent = false;
+      this.GRID.material.transparent = true;
       this.GRID.receiveShadow = true;
 
       this.SCENE.add(this.GRID);
@@ -98,9 +134,15 @@ export default class Home extends Vue {
       cube.castShadow = true;
       cube.receiveShadow = true;
 
-      this.SCENE.add(cube);
+      const plane = new Mesh(planeGeometry, planeMaterial);
+      plane.position.y = -1;
+      plane.receiveShadow = true;
 
-      this.RENDERER = new WebGLRenderer();
+      this.SCENE.add(plane);
+
+      this.objects.push(cube);
+
+      this.RENDERER = new WebGLRenderer({ alpha: true });
  
       this.RENDERER.setPixelRatio(window.devicePixelRatio);
       this.RENDERER.setSize( window.innerWidth * 0.8, window.innerHeight * 0.8 );
